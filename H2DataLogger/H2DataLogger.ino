021f7cbf454402134c706dfd0ade83cadcb71b0a
Eugene Lamnek <eugene@geoit.com.au>
2023-05-05 15:30:19 +0930

    Add project files.

---
+++ H2DataLogger/H2DataLogger.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		H2DataLogger.ino
+ Created:	5/5/2023 3:25:38 PM
+ Author:	eugen
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
